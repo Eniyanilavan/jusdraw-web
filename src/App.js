1f4684f1738a873c0bb8ffe57df7c2d9c5f6c379
Eniyanilavan <r.eniyanilavan@gmail.com>
2018-01-29 10:23:10 +0530

    sign out

--- src/App.js
+++ src/App.js
@@ -17,8 +17,7 @@ class App extends Component {
       main:'none',
       islogin:true,
       confirm:'none',
-    }
-
+    };
   }
 
 
@@ -46,7 +45,7 @@ signUp =()=> {
     var attributeEmail = new CognitoUserAttribute(dataEmail);
 
     attributeList.push(attributeEmail);
-
+    const that = this;
     userPool.signUp(parm.uname, parm.pass, attributeList, null, function(err, result){
         if (err) {
             alert(err);
@@ -55,7 +54,7 @@ signUp =()=> {
         var cognitouser = result.user;
         console.log('user name is ' + cognitouser.getUsername());
         alert("successfully created account confirm in email to continue\n"+'user name is ' + cognitouser.getUsername());
-        this.setState({confirm:true,islogin:'none'});
+        that.setState({confirm:true,islogin:'none'});
     });
 }
 confirm = ()=>{
@@ -88,6 +87,7 @@ toconfirm=()=>{
 }
 
 signIn=()=>{
+  const that = this;
   var authenticationData = {
      Username : document.getElementById('signinname').value,
      Password : document.getElementById('signinpassword').value,
@@ -106,7 +106,7 @@ signIn=()=>{
  cognitoUser.authenticateUser(authenticationDetails, {
         onSuccess: function (result) {
             alert('LOGIN SUCCESSFULL');
-            this.setState({main:true,islogin:'none'});
+            that.setState({main:true,islogin:'none'});
           }.bind(this),
         onFailure: function(err){
           alert(err);
@@ -139,117 +139,159 @@ x=()=>{
   reader.readAsDataURL(file)
 }
 
+upload=()=>{
 
-  render() {
-    return (
-      <div>
-        <div className="main" style={{height:"700px",display:this.state.main}}>
-          <header className="header">
-              <div style={{color:'#339933',fontSize:'50px'}}>
-                First Cut<sub>-just as simple</sub>
-              </div>
-          </header>
-          <div>
-            <div style={{width:"50%",float:'left'}}>
-              <div style={{background:'#339933',height:"45px"}}>
-                <div style={{width:"75%",height:"100%",float:"left",textAlign:'center',padding:"10px 0px 0px 110px",color:"white"}}>
-                  Image
-                </div>
-                <button id="img" title="add image" onClick={this.browse}>+</button>
-                <input type="file" id="myFile" name="myFile" style={{display:'none'}} onChange={this.x}/>
-              </div>
-              <div style={{marginTop:"7px",border:"3px dashed gray",width:"75%",marginLeft:"100px",textAlign:"center"}}>
-                <img src={this.state.url} style={{display:this.state.display,width:"100%",height:"100%"}} alt="Uploaded image"/>
-                <p style={{color:"gray",display:this.state.innerTEXT,paddingTop:"60%"}}>IMAGE</p>
-              </div>
-              <button id="upload" onClick={this.upload}>UPLOAD</button>
+}
+componentDidMount(){
+  const that = this;
+  const POOL_DATA = {
+    ClientId : '70qhoo3a6365msckj66ck02fbn',
+    UserPoolId : 'us-east-1_h7Lxkjzd8',
+  }
+  const userPool = new CognitoUserPool(POOL_DATA);
+  const user = userPool.getCurrentUser();
+  if(!user)
+  {
+
+  }
+  else {
+    user.getSession((err, session)=>{
+      if(err){
+
+      }
+      else{
+        if(session.isValid()){
+          that.setState({main:true,islogin:'none'});
+        }
+      }
+    })
+  }
+}
+
+signout = ()=>{
+  const POOL_DATA = {
+    ClientId : '70qhoo3a6365msckj66ck02fbn',
+    UserPoolId : 'us-east-1_h7Lxkjzd8',
+  }
+  const userPool = new CognitoUserPool(POOL_DATA);
+
+  var cognitoUser = userPool.getCurrentUser();
+  cognitoUser.signOut();
+  this.setState({main:'none',islogin:true});
+}
+
+
+render() {
+  return (
+    <div>
+      <div className="main" style={{height:"700px",display:this.state.main}}>
+        <header className="header">
+            <button className="upload" style={{fontSize:"10px",marginLeft:"1200px"}} onClick={this.signout}>SIGN OUT</button>
+            <div style={{color:'#339933',fontSize:'50px'}}>
+              First Cut<sub>-just as simple</sub>
             </div>
-            <div style={{width:"50%",float:'left' }}>
-              <nav style={{background:"#339933",height:"45px"}}>
-                <Tab button="JSON" title="json file"onclick={this.a}/>
-                <Tab button="UI VIEW" title="UI design"onclick={this.c}/>
-              </nav>
-              <textarea name="textjson" style={{display:this.state.text,height:"100%",width:"99%"}}></textarea>
-              <textarea name="textjs" style={{display:this.state.ui,height:"100%",width:"99%"}}></textarea>
+        </header>
+        <div>
+          <div style={{width:"50%",float:'left'}}>
+            <div style={{background:'#339933',height:"45px"}}>
+              <div style={{width:"75%",height:"100%",float:"left",textAlign:'center',padding:"10px 0px 0px 110px",color:"white"}}>
+                Image
+              </div>
+              <button id="img" title="add image" onClick={this.browse}>+</button>
+              <input type="file" id="myFile" name="myFile" style={{display:'none'}} onChange={this.x}/>
             </div>
+            <div style={{marginTop:"7px",border:"3px dashed gray",width:"75%",marginLeft:"100px",textAlign:"center"}}>
+              <img src={this.state.url} style={{display:this.state.display,width:"100%",height:"100%"}} alt="Uploaded image"/>
+              <p style={{color:"gray",display:this.state.innerTEXT,paddingTop:"60%"}}>IMAGE</p>
+            </div>
+            <button className="upload" onClick={this.upload}>UPLOAD</button>
+          </div>
+          <div style={{width:"50%",float:'left' }}>
+            <nav style={{background:"#339933",height:"45px"}}>
+              <button className="Disp" title="json file" onClick={this.a}>JSON</button>
+              <button className="Disp" title="UI design" onClick={this.c}>UI VIEW</button>
+            </nav>
+            <textarea name="textjson" style={{display:this.state.text,height:"100%",width:"99%"}}></textarea>
+            <textarea name="textjs" style={{display:this.state.ui,height:"100%",width:"99%"}}></textarea>
           </div>
         </div>
-        <div style={{display:this.state.islogin}}>
-          <div className="App">
-            <header className="App-header">
-              <img src="http://www.myiconfinder.com/uploads/iconsets/a4907a7eba6cd4ac85f0762f94fa48e4.png" className="App-logo" alt="logo" />
-              <h1 className="App-title">Welcome to JusDraw</h1>
-              <p className="App-intro">
-                Draw() Develop() Deploy()
-              </p>
-            </header>
-            <div className = "MainDiv">
-              <div className = "SignUpDiv">
-                <Row>
-                    <b>SIGNUP</b>
-                </Row>
-                <Row>
-                    <Input  id= "Username" s={12} label="User Name" validate><Icon>contacts</Icon></Input>
-                </Row>
-                <Row>
-                    <Input  id="Emailid" s={12} label="Email" validate type='email'><Icon>email</Icon></Input>
-                </Row>
-                <Row>
-                    <Input  id="Password" s={12} label="Password" validate type = 'password'><Icon>lock</Icon></Input>
-                </Row>
-                <Row>
-                    <Button waves='light' onClick = {this.signUp}>SIGNUP</Button>
-                </Row>
-              </div>
-              <div className = "SignInDiv">
-                <Row>
-                    <b>SIGNIN</b>
-                </Row>
-                <Row>
-                    <Input  id="signinname" s={12} label="Username" validate><Icon>contacts</Icon></Input>
-                </Row>
-                <Row>
-                    <Input  id="signinpassword" s={12} label="Password" validate type = 'password'><Icon>lock</Icon></Input>
-                </Row>
-                <br/><br/><br/><br/>
-                <Row>
-                    <Button waves='light' onClick = {this.signIn}>button</Button>
-                </Row>
-              </div>
-            </div>
-          </div>
-          <Button waves='light' onClick = {this.toconfirm} style={{marginLeft:"650px"}}>CONFIRM USER</Button>
-        </div>
-        <div style={{display:this.state.confirm}}>
-          <div className="App">
-            <header className="App-header">
-            <img src="http://www.myiconfinder.com/uploads/iconsets/a4907a7eba6cd4ac85f0762f94fa48e4.png" className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to JusDraw</h1>
-            <p className="App-intro">
+      </div>
+      <div style={{display:this.state.islogin}}>
+        <div className="app">
+          <header className="app-header">
+            <img src="http://www.myiconfinder.com/uploads/iconsets/a4907a7eba6cd4ac85f0762f94fa48e4.png" className="app-logo" alt="logo" />
+            <h1 className="app-title">Welcome to JusDraw</h1>
+            <p className="app-intro">
               Draw() Develop() Deploy()
             </p>
-            </header>
-            <div className = "MainDiv">
-              <div style={{marginTop:"10px",width:"500px"}}>
-                <br/>
-                <Row>
-                    <b>CONFIRMATION</b>
-                </Row>
-                <Row>
-                    <Input  id= "username" s={12} label="User Name" validate><Icon>contacts</Icon></Input>
-                </Row>
-                <Row>
-                    <Input  id="confirm" s={12} label="confirmation code" validate type = 'text'><Icon>lock</Icon></Input>
-                </Row>
-                <Row>
-                    <Button waves='light' onClick = {this.confirm}>CONFIRM</Button>
-                </Row>
-              </div>
+          </header>
+          <div className = "MainDiv">
+            <div className = "SignUpDiv">
+              <Row>
+                  <b>SIGNUP</b>
+              </Row>
+              <Row>
+                  <Input  id= "Username" s={12} label="User Name" validate><Icon>contacts</Icon></Input>
+              </Row>
+              <Row>
+                  <Input  id="Emailid" s={12} label="Email" validate type='email'><Icon>email</Icon></Input>
+              </Row>
+              <Row>
+                  <Input  id="Password" s={12} label="Password" validate type = 'password'><Icon>lock</Icon></Input>
+              </Row>
+              <Row>
+                  <Button waves='light' onClick = {this.signUp}>SIGNUP</Button>
+              </Row>
+            </div>
+            <div className = "SignInDiv">
+              <Row>
+                  <b>SIGNIN</b>
+              </Row>
+              <Row>
+                  <Input  id="signinname" s={12} label="Username" validate><Icon>contacts</Icon></Input>
+              </Row>
+              <Row>
+                  <Input  id="signinpassword" s={12} label="Password" validate type = 'password'><Icon>lock</Icon></Input>
+              </Row>
+              <br/><br/><br/><br/>
+              <Row>
+                  <Button waves='light' onClick = {this.signIn}>button</Button>
+              </Row>
+            </div>
+          </div>
+        </div>
+        <Button waves='light' onClick = {this.toconfirm} style={{marginLeft:"650px"}}>CONFIRM USER</Button>
+      </div>
+      <div style={{display:this.state.confirm}}>
+        <div className="app">
+          <header className="app-header">
+          <img src="http://www.myiconfinder.com/uploads/iconsets/a4907a7eba6cd4ac85f0762f94fa48e4.png" className="app-logo" alt="logo" />
+          <h1 className="app-title">Welcome to JusDraw</h1>
+          <p className="App-intro">
+            Draw() Develop() Deploy()
+          </p>
+          </header>
+          <div className = "MainDiv">
+            <div style={{marginTop:"10px",width:"500px"}}>
+              <br/>
+              <Row>
+                  <b>CONFIRMATION</b>
+              </Row>
+              <Row>
+                  <Input  id= "username" s={12} label="User Name" validate><Icon>contacts</Icon></Input>
+              </Row>
+              <Row>
+                  <Input  id="confirm" s={12} label="confirmation code" validate type = 'text'><Icon>lock</Icon></Input>
+              </Row>
+              <Row>
+                  <Button waves='light' onClick = {this.confirm}>CONFIRM</Button>
+              </Row>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 }
 export default App;
